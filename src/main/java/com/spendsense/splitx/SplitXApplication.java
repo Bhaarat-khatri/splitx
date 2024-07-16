8b9e6935c5e0c064f4d04da2a14f0dffaa37ec84
Khatri <bharatkhatri351@gmail.com>
2024-07-16 22:46:59 +0530

    intial commit

---
+++ src/main/java/com/spendsense/splitx/SplitXApplication.java
@@ -0,0 +1,13 @@
+package com.spendsense.splitx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spendsense/splitx/SplitXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spendsense.splitx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
