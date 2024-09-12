4165185471db83379e4247080f776766b3f1230a
sharad <sharad.19552@knit.ac.in>
2024-09-13 01:05:28 +0530

    Session management configuration

--- src/main/java/com/spendsense/splitx/security/SecurityConfig.java
+++ src/main/java/com/spendsense/splitx/security/SecurityConfig.java
@@ -6,32 +6,55 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 import com.spendsense.splitx.service.CustomAuthenticationSuccessHandler;
+import com.spendsense.splitx.service.CustomInvalidSessionStrategy;
 
 @Configuration
 @EnableMethodSecurity
 @EnableWebSecurity
 public class SecurityConfig {
-	
-	 private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    
+    private final CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    private final CustomInvalidSessionStrategy customInvalidSessionStrategy;
 
-	    public SecurityConfig(CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler) {
-	        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
-	    }
-	@Bean
-	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-		http.csrf(AbstractHttpConfigurer::disable)
-				.authorizeHttpRequests(authorizeRequest -> authorizeRequest.anyRequest().authenticated())
-				.oauth2Login(oauth2 -> oauth2.successHandler(customAuthenticationSuccessHandler))
-				.logout(logout -> logout
-		                .logoutUrl("/logout") // Define the logout URL
-		                .invalidateHttpSession(true) // Invalidate session
-		                .deleteCookies("JSESSIONID") // Delete cookies
-		            );
-		
+    public SecurityConfig(CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler, CustomInvalidSessionStrategy customInvalidSessionStrategy) {
+        this.customAuthenticationSuccessHandler = customAuthenticationSuccessHandler;
+        this.customInvalidSessionStrategy = customInvalidSessionStrategy;
+    }
 
-		return http.build();
-	}
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequest -> authorizeRequest.anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2.successHandler(customAuthenticationSuccessHandler))
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // Define the logout URL
+                        .invalidateHttpSession(true) // Invalidate session
+                        .deleteCookies("JSESSIONID") // Delete cookies
+                )
+                .sessionManagement(session -> session
+                		.invalidSessionStrategy(customInvalidSessionStrategy)
+                        .maximumSessions(1) // Allow one session per user
+                )
+                .sessionManagement(session -> session
+                        .sessionFixation().migrateSession() // Handle session fixation
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) // Session creation policy
+                );
+
+        return http.build();
+    }
+
+    // Extend JSESSIONID cookie validity to 1 year
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setCookieMaxAge(31536000); // 1 year in seconds
+        serializer.setCookieName("JSESSIONID");
+        return serializer;
+    }
 }
--- src/main/java/com/spendsense/splitx/service/UserInterceptor.java
+++ src/main/java/com/spendsense/splitx/service/UserInterceptor.java
@@ -10,49 +10,48 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import com.spendsense.splitx.entity.User;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Component
 public class UserInterceptor implements HandlerInterceptor {
 
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        // Step 1: Verify the session (implicitly done by Spring Security)
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// Step 1: Verify the session (implicitly done by Spring Security)
 
-        // Step 2: Get the email from the authenticated user
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        Object principal = authentication.getPrincipal();
-        String email = "";
+		// Step 2: Get the email from the authenticated user
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		Object principal = authentication.getPrincipal();
+		String email = "";
 
-        if (principal instanceof OidcUser) {
-            // For OIDC authentication (OpenID Connect)
-            OidcUser oidcUser = (OidcUser) principal;
-            email =  oidcUser.getEmail(); // Direct method for getting email
-        } else if (principal instanceof OAuth2User) {
-            // For OAuth2 authentication
-            OAuth2User oauth2User = (OAuth2User) principal;
-            Map<String, Object> attributes = oauth2User.getAttributes();
-            email =  (String) attributes.get("email");
-        } else {
-            // Fallback case
-            throw new IllegalStateException("Unsupported principal type: " + principal.getClass().getName());
-        }
-        
-        // This should be the email if you're using email as the principal
-        System.out.println("Inside interceptor" + email);
-        // Step 3: Get user ID from the database using the email
-        Long userId = userService.findUserByEmail(email).getUserId();
-        
-        System.out.println("dfs" + userId + email);
-        // Step 4: Add the user ID to the request
-        request.setAttribute("userId", userId);
+		if (principal instanceof OidcUser) {
+			// For OIDC authentication (OpenID Connect)
+			OidcUser oidcUser = (OidcUser) principal;
+			email = oidcUser.getEmail(); // Direct method for getting email
+		} else if (principal instanceof OAuth2User) {
+			// For OAuth2 authentication
+			OAuth2User oauth2User = (OAuth2User) principal;
+			Map<String, Object> attributes = oauth2User.getAttributes();
+			email = (String) attributes.get("email");
+		} else {
+			// Fallback case
+			throw new IllegalStateException("Unsupported principal type: " + principal.getClass().getName());
+		}
 
-        return true; // Continue processing the request
-    }
+		// This should be the email if you're using email as the principal
+		System.out.println("Inside interceptor" + email);
+		// Step 3: Get user ID from the database using the email
+		Long userId = userService.findUserByEmail(email).getUserId();
+
+		System.out.println("dfs" + userId + email);
+		// Step 4: Add the user ID to the request
+		request.setAttribute("userId", userId);
+
+		return true; // Continue processing the request
+	}
 }
